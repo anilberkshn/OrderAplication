68b671e4b2ada1ce114b19010e85fe061221e3f9
anilberkshn <117069621+anilberkshn@users.noreply.github.com>
2023-06-09 23:36:00 +0300

    OrderCase Unversioned Files

---
+++ Customer/Startup.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Customer.Config;
+using Customer.Database.Context;
+using Customer.Database.Interface;
+using Customer.Middlewares;
+using Customer.Repository;
+using Customer.Services;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+
+namespace Customer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete("Obsolete")]
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddFluentValidation(fv=> fv.RegisterValidatorsFromAssemblyContaining<Startup>());
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Customer", Version = "v1" });
+            });
+           
+            var dbSettings = Configuration.GetSection("DatabaseSettings").Get<DatabaseSettings>();
+            var client = new MongoClient(dbSettings.ConnectionString);
+            var context = new Context(client,dbSettings.DatabaseName);
+            
+            services.AddSingleton<IService, Customer.Services.Service>();
+            services.AddSingleton<IContext, Context>(_ => context);
+            services.AddSingleton<IRepository, Repository.Repository>();
+        }
+        
+        
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer v1"));
+            }
+           
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+            
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
